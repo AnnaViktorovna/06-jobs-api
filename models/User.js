ccf24b068f5d9d50d098fa8a4edabb5b7510183a
AnnaViktorovna <anna.bazileeva@gmail.com>
2025-02-05 12:57:45 -0500

    fix login problem

--- models/User.js
+++ models/User.js
@@ -1,53 +1,50 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
+    name: {
+        type: String,
+        unique: true,
+        required: [true, 'Please provide name'],
+        maxlength: 50,
+        minlength: 3,
+    },
     email: {
         type: String,
-        required: [true, "Please provide a email"],
+        required: [true, 'Please provide email'],
         match: [
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            "Please provide a valid email",
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please provide a valid email',
         ],
         unique: true,
     },
     password: {
         type: String,
-        required: [true, "Please provide a password"],
-        minlength: [6, "Password must be at least 6 characters"],
-
+        required: [true, 'Please provide password'],
+        minlength: 6,
     },
-    isSeller: {
-        type: Boolean,
-        default: false,
-    },
-});
+})
 
-UserSchema.pre("save", async function (next) {
-    if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-});
-
-UserSchema.methods.getName = function () {
-    return this.name;
-};
+UserSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
 
 UserSchema.methods.createJWT = function () {
     return jwt.sign(
         { userId: this._id, name: this.name },
         process.env.JWT_SECRET,
         {
-            expiresIn: process.env.JWT_LIFETIME,
+            expiresIn: "24h"
         }
-    );
-};
 
-UserSchema.methods.comparePassword = async function (candidatePassword) {
-    return await bcrypt.compare(candidatePassword, this.password);
-};
+    )
+}
 
+UserSchema.methods.comparePassword = async function (canditatePassword) {
+    const isMatch = await bcrypt.compare(canditatePassword, this.password)
+    return isMatch
+}
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema)
--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-// Define the register function
-const register = (req, res) => {
-    res.send("Registration successful");
-};
+const { register, login } = require("../controllers/auth")
 
-// Define the login function
-const login = (req, res) => {
-    res.send("Login successful");
-};
-
-// Use the functions in your routes directly since they are defined in the same file
 router.post("/register", register);
 router.post("/login", login);
 
